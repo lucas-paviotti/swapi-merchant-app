73183ff5ad4a08442cd739589d7c63721e5dce13
imhest <paviottilucas@gmail.com>
2019-06-14 18:32:44 -0300

    starting to use fetch

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,35 @@ import CardList from '../components/CardList';
 import Order from '../components/Order';
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			starships: []
+		}
+	}
+
+	componentDidMount(){
+	  /*
+		fetch('https://swapi.co/api/starships')
+		.then(response => response.json())
+		.then(ships => {
+			const pages = Math.round(ships.count / 10);
+			for (var i = pages - 1; i >= 0; i--) {
+				if (i !== 1) {
+					fetch('https://swapi.co/api/starships/?page='+ (i + 1))
+					.then(response => response.json())
+					.then(newShips => newShips.results.forEach(e => { ships.results.push(e) }));
+				}
+			}
+			this.setState({ starships: ships });
+		});
+	  */
+
+
+
+
+	}
+
   render() {
     return (
     	<div className='merchant-app'>
