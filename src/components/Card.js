9cb851bbaf78adc8fe9217c5097f1778004c41a0
imhest <paviottilucas@gmail.com>
2019-07-31 15:18:16 -0300

    Added responsive images

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,10 @@ class Card extends React.Component {
 		const { name, model, manufacturer, cost_in_credits, length, crew, passengers, cargo_capacity } = this.props;
 		return (
 			<div className="card" onClick={() => this.props.addToOrder(this.props.index)}>
-				<div>
+				<div className="card-image">
+					<img src="https://s-media-cache-ak0.pinimg.com/736x/b3/32/b1/b332b1221cc8ff848aa91982743576f4.jpg" alt="a baby duckling sitting on its duckling butt"/>
+				</div>
+				<div className="card-info">
 					<h2>{name}</h2>
 					<p>Model: {model}</p>
 					<p>Manufacturer: {manufacturer}</p>
