7565dc4024174a06a5f6fa76a592791108cdc841
imhest <paviottilucas@gmail.com>
2019-08-02 12:14:38 -0300

    Updates

---
+++ precache-manifest.c2b28e5a24c5d3110d66c4f758982a61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5622ced437a173eb1e5ef8c8c16a921d",
+    "url": "/swapi-merchant-app/index.html"
+  },
+  {
+    "revision": "13bcec366805c08e44c4",
+    "url": "/swapi-merchant-app/static/css/main.cd1834c5.chunk.css"
+  },
+  {
+    "revision": "33a8f4a9e44d5822ec4f",
+    "url": "/swapi-merchant-app/static/js/2.0776aad9.chunk.js"
+  },
+  {
+    "revision": "13bcec366805c08e44c4",
+    "url": "/swapi-merchant-app/static/js/main.78ae21f7.chunk.js"
+  },
+  {
+    "revision": "3786ccd48429069e47c2",
+    "url": "/swapi-merchant-app/static/js/runtime~main.49148ff8.js"
+  },
+  {
+    "revision": "087cf85fa23c47b98ae639638a9d566b",
+    "url": "/swapi-merchant-app/static/media/NewsGothicStd.087cf85f.otf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/swapi-merchant-app/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/swapi-merchant-app/static/media/stars.6494ab5b.png"
+  }
+]);
